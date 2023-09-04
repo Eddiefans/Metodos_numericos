aa3a7a41c92e24e2cd3475e3e9a184a3d048feeb
Eddie Aguilar Ceballos <edaguilarce@gmail.com>
2023-09-04 00:26:11 -0600

    Create secante.m

---
+++ secante.m
@@ -0,0 +1,7 @@
+function secante
+M = ["Iteracion" "x0" "x1" "f(x0)" "f(x1)" "x2" "Err"]
+y = input("Ingresa la funcion a resolver: ", "s")
+x0 = input("Ingresa tu valor inicial: ")
+Er = input("Ingresa el error que quieres alcanzar: ")
+
+end

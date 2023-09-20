41bba016f0749ee9c65c1f0fbb3f8b4a2b453cac
Eddie Aguilar Ceballos <edaguilarce@gmail.com>
2023-09-20 14:03:52 -0600

    Metodos

--- metodos_numericos_proj/jacobi.py
+++ metodos_numericos_proj/jacobi.py
@@ -1,11 +1,13 @@
 def fx(y, z):
-    return (8 + y - 3*z)/12
+    return (8 + y - 3 * z) / 12
+
 
 def fy(x, z):
-    return (-51 - x + 4*z)/7
+    return (-51 - x + 4 * z) / 7
+
 
 def fz(x, y):
-    return (62 -4*x + 4*y)/9
+    return (62 - 4 * x + 4 * y) / 9
 
 
 x, y, z, i = 0, 0, 0, 0
@@ -16,24 +18,22 @@ while True:
     ya = y
     za = z
     print("Iteración = {}, x = {}, y = {}, z = {}".format(i, x, y, z))
-    
+
     x = round(fx(ya, za), 4)
     y = round(fy(xa, za), 4)
     z = round(fz(xa, ya), 4)
-    
-    erx = abs(x-xa)/x
-    ery = abs(y-ya)/y
-    erz = abs(z-za)/z
-    
+
+    erx = abs(x - xa) / x
+    ery = abs(y - ya) / y
+    erz = abs(z - za) / z
+
     i += 1
-    
+
     if (erx < 0.003) and (ery < 0.003) and (erz < 0.003):
         break
-    
-    
+
 print("Iteración = {}, x = {}, y = {}, z = {}".format(i, x, y, z))
 print("Errores: x = {}, y = {}, z = {}".format(erx, ery, erz))
-
 
 x, y, z, i = 0, 0, 0, 0
 
@@ -43,20 +43,19 @@ while True:
     ya = y
     za = z
     print("Iteración = {}, x = {}, y = {}, z = {}".format(i, x, y, z))
-    
+
     x = round(fx(ya, za), 4)
     y = round(fy(x, za), 4)
     z = round(fz(x, y), 4)
-    
-    erx = abs(x-xa)/x
-    ery = abs(y-ya)/y
-    erz = abs(z-za)/z
-    
+
+    erx = abs(x - xa) / x
+    ery = abs(y - ya) / y
+    erz = abs(z - za) / z
+
     i += 1
-    
+
     if (erx < 0.003) and (ery < 0.003) and (erz < 0.003):
         break
-    
-    
+
 print("Iteración = {}, x = {}, y = {}, z = {}".format(i, x, y, z))
 print("Errores: x = {}, y = {}, z = {}".format(erx, ery, erz))
